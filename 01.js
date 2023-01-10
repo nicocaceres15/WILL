cf8db2a03b4d07eb354ef79760d16c2f4b24caa0
¨Nicolas <nicolasalejandrocaceres.1595@gmail.com>
2023-01-10 01:23:30 -0300

    ya termine el ejercicio 01

--- 01.js
+++ 01.js
@@ -11,7 +11,13 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+  let nuevoArray = [];
+  for (var i = 0; i < array.length; i++){
+    if (array[i] > 0){
+      nuevoArray.push(array[i])
+    }
+  }
+  return nuevoArray;
 }
 
 // No modifiques nada debajo de esta linea //
